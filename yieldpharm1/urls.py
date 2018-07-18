49c13cf6780adeb0900221e3498b1c15735e3802
Jinyu Guo <whatislinux@outlook.com>
2018-07-18 22:15:15 +0800

    add translate

--- yieldpharm1/urls.py
+++ yieldpharm1/urls.py
@@ -12,4 +12,15 @@ urlpatterns=[
     url(r'^lab/$',views.lab),
     url(r'^news/$',views.news),
     url(r'^feedback/$',views.feedback),
+    url(r'^en/$',views.indexen),
+    url(r'^en/(?P<ydclass_id>[0-9]+)/$', views.ydclassen),
+    url(r'^en/(?P<product_id>[0-9]+)/detail$', views.detailen),
+    url(r'^en/search/$', views.searchen),
+    url(r'^en/aboutus/$',views.aboutusen),
+    url(r'^en/contactus/$',views.contactusen),
+    url(r'^en/products/$', views.productsen),
+    url(r'^en/lab/$', views.laben),
+    url(r'^en/news/$', views.newsen),
+    url(r'^en/feedback/$', views.feedbacken),
+
 ]
--- yieldpharm1/views.py
+++ yieldpharm1/views.py
@@ -52,3 +52,52 @@ def news(request):
 def feedback(request):
     category_list = Category.objects.all()
     return render(request, 'yieldpharm1/feedback.html', {'category_list': category_list})
+
+def indexen(request):
+    category_list = Category.objects.all()
+    # print(category_list)
+    return render(request, 'yieldpharm1/en/index.html', {'category_list': category_list})
+
+def aboutusen(request):
+    category_list = Category.objects.all()
+    return render(request, 'yieldpharm1/en/aboutus.html', {'category_list': category_list})
+
+def contactusen(request):
+    category_list = Category.objects.all()
+    return render(request, 'yieldpharm1/en/contactus.html', {'category_list': category_list})
+
+def productsen(request):
+    category_list = Category.objects.all()
+    product_list = Product.objects.all()
+    return render(request, 'yieldpharm1/en/products.html', {'product_list': product_list, 'category_list': category_list})
+
+def laben(request):
+    category_list = Category.objects.all()
+    return render(request, 'yieldpharm1/en/lab.html', {'category_list': category_list})
+
+def newsen(request):
+    category_list = Category.objects.all()
+    return render(request, 'yieldpharm1/en/news.html', {'category_list': category_list})
+
+def feedbacken(request):
+    category_list = Category.objects.all()
+    return render(request, 'yieldpharm1/en/feedback.html', {'category_list': category_list})
+
+def ydclassen(request,ydclass_id):
+    category_list = Category.objects.all()
+    product_list = Product.objects.filter(category_id=ydclass_id)
+    return render(request, 'yieldpharm1/en/ydclass.html', {'product_list': product_list, 'category_list': category_list})
+
+def detailen(request,product_id):
+    category_list = Category.objects.all()
+    product = Product.objects.get(pk=product_id)
+    # print(category_list)
+    return render(request, 'yieldpharm1/en/detail.html', {'product': product, 'category_list': category_list})
+
+def searchen(request):
+    category_list = Category.objects.all()
+    search_text=request.POST.get('search_text')
+    product_list=Product.objects.filter(Q(name__icontains=search_text)|Q(cas_no__icontains=search_text))
+    # print(search_text,product_list)
+    return render(request,'yieldpharm1/en/search.html',{'product_list':product_list,'category_list':category_list})
+
