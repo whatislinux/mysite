5113580156640e7797b07f972d3d034fc7a90fa4
Jinyu Guo <whatislinux@outlook.com>
2018-07-26 23:52:35 +0800

    version1 finish

--- yieldpharm1/models.py
+++ yieldpharm1/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class Category(models.Model):
-    name=models.CharField(max_length=32)
-    
+    name=models.CharField(max_length=200)
+    name_en=models.CharField(max_length=200)
     def __str__(self):
         return self.name
 
@@ -14,11 +14,13 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name=models.CharField(max_length=32)
-    cas_no=models.CharField(max_length=32)
-    molecular_ormula=models.CharField(max_length=32)
+    name=models.CharField(max_length=200)
+    name_en=models.CharField(max_length=200)
+    cas_no=models.CharField(max_length=200)
+    molecular_formula=models.CharField(max_length=200)
     molecular_weight=models.FloatField()
-    molecular_structure=models.CharField(max_length=128)
+    molecular_structure=models.CharField(max_length=200)
+    is_advantage_product=models.IntegerField(default=0)
     category=models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
 
     def __str__(self):
